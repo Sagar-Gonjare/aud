b4aac5a3ff11250449dfad54ef2af9d665446972
Sagar Gonjare <sagargonjare2004@gmail.com>
2024-04-20 15:15:21 +0530

    impl: Implement db migration
        * implement db migration using liquibase

---
+++ src/main/java/org/dnyanyog/dto/AuditLogResponse.java
@@ -0,0 +1,46 @@
+package org.dnyanyog.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.stereotype.Component;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Component
+public class AuditLogResponse {
+
+  private String status;
+
+  private String message;
+
+  private AuditLog data;
+
+  public static AuditLogResponse getInstance() {
+    return new AuditLogResponse();
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public AuditLogResponse setStatus(String status) {
+    this.status = status;
+    return this;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public AuditLogResponse setMessage(String message) {
+    this.message = message;
+    return this;
+  }
+
+  public AuditLog getData() {
+    return data;
+  }
+
+  public AuditLogResponse setData(AuditLog data) {
+    this.data = data;
+    return this;
+  }
+}
